91ff667171caa3122b4a6a096931ed54ae2dd39d
Murilo <murilo.p.miorim@gmail.com>
2024-08-05 09:12:08 -0300

    initializing api

--- src/screens/menu-sections/CadastrarReceptora.js
+++ src/screens/menu-sections/CadastrarReceptora.js
@@ -1,59 +1,51 @@
-import React, {useState} from "react";
-import { Text, TextInput, View, TouchableOpacity } from "react-native";
-
+import React, { useState } from "react";
+import { Text, TextInput, View, TouchableOpacity, Alert } from "react-native";
 import AntDesign from '@expo/vector-icons/AntDesign';
-
 import style from "../../components/style";
 
-export const newReceiverName = ''
-export const newReceiverBreed = ''
-export const newReceiverIndentification = ''
-
 export default ({ navigation }) => {
-    const [newReceiverName, setName] = useState(newReceiverName)
-    const [newReceiverBreed, setBreed] = useState(newReceiverBreed)
-    const [newReceiverIndentification, setNumber] = useState(newReceiverIndentification)
+    const [newReceiverName, setName] = useState('');
+    const [newReceiverBreed, setBreed] = useState('');
+    const [newReceiverIdentification, setIdentification] = useState('');
 
     return (
         <View style={style.menu}>
             <View style={style.divTitle}>
-                <TouchableOpacity onPress={() => {
-                    navigation.navigate('Menu')
-                }}>
-                    <View style={{marginRight: 50}}>
-                    <AntDesign name="arrowleft" size={24} color="#fff" />
+                <TouchableOpacity onPress={() => navigation.navigate('Menu')}>
+                    <View style={{ marginRight: 50 }}>
+                        <AntDesign name="arrowleft" size={24} color="#fff" />
                     </View>
                 </TouchableOpacity>
                 <Text style={style.titleText}>Cadastro da Receptora</Text>
             </View>
             <View style={style.content}>
                 <Text style={style.label}>Nome:</Text>
-                <TextInput                     
+                <TextInput
                     placeholder="Nome da Receptora"
                     value={newReceiverName}
                     style={style.input}
-                    onChangeText={(newReceiverName => setName(newReceiverName))}
+                    onChangeText={setName}
                 />
                 <Text style={style.label}>Raça:</Text>
-                <TextInput 
+                <TextInput
                     placeholder="Raça da Receptora"
                     value={newReceiverBreed}
                     style={style.input}
-                    onChangeText={(newReceiverBreed => setBreed(newReceiverBreed))}
+                    onChangeText={setBreed}
                 />
                 <Text style={style.label}>Identificação:</Text>
-                <TextInput 
+                <TextInput
                     placeholder="Identificação da Receptora"
-                    value={newReceiverIndentification}
+                    value={newReceiverIdentification}
                     style={style.input}
-                    onChangeText={(newReceiverIndentification => setNumber(newReceiverIndentification))}
+                    onChangeText={setIdentification}
                 />
-                <View >
-                    <TouchableOpacity style={style.button} onPress={() => alert(`Salvo com sucesso!`)}>
+                <View>
+                    <TouchableOpacity style={style.button} onPress={() => {}}>
                         <Text style={style.buttonText}>Salvar</Text>
                     </TouchableOpacity>
                 </View>
             </View>
         </View>
-    )
-}
+    );
+};
